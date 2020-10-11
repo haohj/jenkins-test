604c51510cac24c4f3e7ec0cc5f57271de2b88e9
haohj <haohj94@163.com>
2020-10-11 19:12:45 +0800

    init projects

---
+++ src/main/java/com/hao/jenkins/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.hao.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/index")
+    @ResponseBody
+    public String index() {
+        return "hello jenkins";
+    }
+}
